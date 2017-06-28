869acece01e29bbbc3e8350b13517081db62f80d
Hemant Pandey <hpscjp@gmail.com>
2017-06-28 12:23:19 +0530

    Update SelectionSort.cpp

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -27,7 +27,7 @@ int main()
 void Selection_Sort(int a[],int n){
 	int i,j,m,temp;
 	for(i=0;i<n;i++){
-		m=0;
+		m=i;
 		for(j=0;j<n-i;j++){
 			if(a[m]<=a[j])
 				m=j;
@@ -37,3 +37,28 @@ void Selection_Sort(int a[],int n){
 		a[m]=temp;
 	}
 }
+
+
+//Hackerearth code of selection sorting
+void selection_sort (int A[ ], int n) {
+        // temporary variable to store the position of minimum element
+
+        int minimum;        
+        // reduces the effective size of the array by one in  each iteration.
+
+        for(int i = 0; i < n-1 ; i++)  {
+
+           // assuming the first element to be the minimum of the unsorted array .
+             minimum = i ;
+
+          // gives the effective size of the unsorted  array .
+
+            for(int j = i+1; j < n ; j++ ) {
+                if(A[ j ] < A[ minimum ])  {                //finds the minimum element
+                minimum = j ;
+                }
+             }
+          // putting minimum element on its proper position.
+          swap ( A[ minimum ], A[ i ]) ; 
+        }
+   }
