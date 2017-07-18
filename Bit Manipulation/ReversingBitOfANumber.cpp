ab343961bc8a6c65a6757923abe6e33c2f81f7e7
Hemant Pandey <hpscjp@gmail.com>
2017-07-18 10:03:21 +0530

    Create ReversingBitOfANumber.cpp

---
+++ Bit Manipulation/ReversingBitOfANumber.cpp
@@ -0,0 +1,30 @@
+/*
+Reverse bits of an 32 bit unsigned integer
+
+Example 1:
+
+x = 0,
+
+          00000000000000000000000000000000  
+=>        00000000000000000000000000000000
+return 0
+
+Example 2:
+
+x = 3,
+
+          00000000000000000000000000000011 
+=>        11000000000000000000000000000000
+return 3221225472
+*/
+
+
+int ReverseBit(int n){
+	int num=0;
+	for(int i=0;i<32;i++){
+		int b=n & (1<<i);
+		if(b)
+			num|=1<<(31-i);
+	}
+	return num;
+}
